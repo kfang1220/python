f91ef618b4fb86081e1064f6ef547964bd4dcb1d
kevinfang <fangk1220@gmail.com>
2019-06-28 23:01:44 -0700

    first python file

---
+++ python_learning.py
@@ -0,0 +1,29 @@
+class question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+question_prompts =[
+    "What color are apples?\n(a) Red\n(b) Purple\n(c) Orange \n\n",
+    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+
+questions = [
+    question(question_prompts[0], "a"),
+    question(question_prompts[1], "c"),
+    question(question_prompts[2], "b"),
+
+]
+
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score +=1
+    print("You got " + str(score) + "/" + str(len(question_prompts)) + " Correct")
+
+run_test(questions)
